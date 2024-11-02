dff159d08bd20c0de5b0880b4f3df3f7c28c3559
Krakers42 <krakowskidariusz@wp.pl>
2024-11-02 20:12:14 +0100

    Gotowe laboratorium 1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,67 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+abstract class Shape {
+    public abstract double getArea();
+    public abstract double getPerimeter();
+}
+
+class Rectangle extends Shape {
+    private double width;
+    private double height;
+
+    public Rectangle (double width, double height){
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return width * height;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * width + 2 * height;
+    }
+}
+
+class Triangle extends Shape {
+    private double a, b, c;
+
+    public Triangle (double a, double b, double c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    @Override
+    public double getArea() {
+        double p = getPerimeter() / 2;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+
+    @Override
+    public double getPerimeter() {
+        return a + b + c;
+    }
+}
+
+class ShapeDescriber {
+    public void describe (Shape shape){
+        System.out.println("Pole kształtu: " + shape.getArea());
+        System.out.println("Obwód kształtu: " + shape.getPerimeter());
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Rectangle rectangle = new Rectangle(3.0, 4.0);
+        Triangle triangle = new Triangle(5.0, 6.0, 7.0);
+        ShapeDescriber describer = new ShapeDescriber();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Prostokąt");
+        describer.describe(rectangle);
+
+        System.out.println("\nTrójkąt");
+        describer.describe(triangle);
     }
 }
